2d272f3c47dca6bc30656b51b1a772fe77e3bf71
Sanskar <sanskarofficial30@gmail.com>
2026-04-15 12:00:58 +0530

    Added Day2 Java files

---
+++ PracticeProblem/Day2/SumNaturalNumbers.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+            System.out.print(i);
+            if (i < n)
+                System.out.print("+");
+        }
+
+        System.out.print(" = " + sum);
+    }
+}
