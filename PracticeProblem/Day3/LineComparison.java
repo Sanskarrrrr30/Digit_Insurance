f42c08025ddd169ed644e24f1d34ba2f7c7641be
Sanskar <sanskarofficial30@gmail.com>
2026-04-15 16:21:43 +0530

    Added UC2: Check Line equality

--- PracticeProblem/Day3/LineComparison.java
+++ PracticeProblem/Day3/LineComparison.java
@@ -1,20 +1,45 @@
-//UC1
+//UC2
 import java.util.*;
 
 class LineComparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+        int x1 = sc.nextInt(), y1 = sc.nextInt();
+        int x2 = sc.nextInt(), y2 = sc.nextInt();
 
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        int x3 = sc.nextInt(), y3 = sc.nextInt();
+        int x4 = sc.nextInt(), y4 = sc.nextInt();
 
-        System.out.println("Length = " + length);
+        double line1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double line2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
+        if(Double.valueOf(line1).equals(line2))
+            System.out.println("Lines are Equal");
+        else
+            System.out.println("Lines are Not Equal");
     }
 }
+
+
+
+//UC1
+// import java.util.*;
+
+// class LineComparison {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int x1 = sc.nextInt();
+//         int y1 = sc.nextInt();
+//         int x2 = sc.nextInt();
+//         int y2 = sc.nextInt();
+
+//         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+//         System.out.println("Length = " + length);
+//     }
+// }
 
 
 
