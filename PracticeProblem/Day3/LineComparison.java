a9c0a59e2acdcdd569d931918e37bc544c887ccd
Sanskar <sanskarofficial30@gmail.com>
2026-04-15 16:18:26 +0530

    Added UC1: Calculate Line Length

--- PracticeProblem/Day3/LineComparison.java
+++ PracticeProblem/Day3/LineComparison.java
@@ -1,5 +1,26 @@
+//UC1
+import java.util.*;
+
 class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("Length = " + length);
     }
 }
+
+
+
+//UC0
+// class LineComparison {
+//     public static void main(String[] args) {
+//         System.out.println("Welcome to Line Comparison Computation Program");
+//     }
+// }
